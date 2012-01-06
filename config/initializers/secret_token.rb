b65bdce29d9c8b139772bc97be8eb58d72b14628
Jarek <banyaai[at]gmail.com>
2012-01-06 22:04:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaiCentrala::Application.config.secret_token = '516ca0c70e9dced2bd3e08e4cb245d71a357d9ecf9236ff0f70d60ae9f56e329504568c65c19d708b1608f95468ed1c69df13d68a1df1bc6c9b95416cac5163a'
