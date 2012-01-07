9130d7abedcd1b27e5924e9574145e44c5e7c2ba
Jarek <banyaai[at]gmail.com>
2012-01-07 22:20:16 +0100

    user signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,12 +6,12 @@ describe UsersController do
   describe "GET 'new'" do
 
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
@@ -48,4 +48,52 @@ describe UsersController do
     end
   end
 
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", :email => "ueser@example.com", :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the usser show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to centrala app/i
+      end
+    end
+  end
 end
